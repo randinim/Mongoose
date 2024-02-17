52b01fd93a64cf53284d8fc1c3d4bd70d6a27fe6
Randini Maliksha <maliksharandini@gmail.com>
2024-02-17 12:39:02 +0530

    read

--- 2.CRUD-operations/server.js
+++ 2.CRUD-operations/server.js
@@ -62,22 +62,95 @@ const User = mongoose.model("User", userProfileSchema);
 //   })
 //   .catch((error) => console.log(error));
 //! ----.create()--------
-User.create({
-  username: "emmanuel",
-  age: 26,
-  birthday: new Date("2001-04-15"),
-  isActive: true,
-  hobbies: ["Soccer", "Reading", "Coding"],
-  address: {
-    street: "789 0ak St",
-    city: "Kumasi",
-    postaclCode: 5551,
-  },
-  customdata: {
-    country: "Ghana",
-  },
-})
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+// User.create({
+//   username: "emmanuel",
+//   age: 26,
+//   birthday: new Date("2001-04-15"),
+//   isActive: true,
+//   hobbies: ["Soccer", "Reading", "Coding"],
+//   address: {
+//     street: "789 0ak St",
+//     city: "Kumasi",
+//     postaclCode: 5551,
+//   },
+//   customdata: {
+//     country: "Ghana",
+//   },
+// })
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+//! ----.find()--------
+// User.find()
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+//! ----.findOne()--------
+// User.findOne({
+//   username: "masynctech",
+// })
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+// //! ----.findById()--------
+// User.findById("652fcb47a0fc777e4baba1e5")
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+//! ----.where()--------
+// const findUsers = async () => {
+//   try {
+//     const users = await User.find().where("age").gte(27);
+//     console.log(users);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+// findUsers();
+//!---------.sort----------
+// const findUsers = async () => {
+//   try {
+//     const users = await User.find()
+//       .where("age")
+//       .gte(20)
+//       .sort({ username: 1 })
+//       .limit(1);
+//     console.log(users);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+// findUsers();
+//!=====UPDATING DOCUMENTS====
+//!----.updateOne()------
+// const updateOneFn = async () => {
+//   try {
+//     const updatedDoc = await User.updateOne(
+//       { name: "Prince" },
+//       { email: "prince2@gmail.com", age: 30 },
+//       { new: true }
+//     );
+//     console.log(updatedDoc);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+// updateOneFn();
+
+//!----.findByIdAndUpdate()---------
+const updateDoc = async () => {
+  try {
+    const updatedDoc = await User.findByIdAndUpdate(
+      "65cb4beb9721168b98a8ae34",
+      { age: "40" },
+      { email: "prince2@gmail.com", age: 30 },
+      { username: "Thomas" },
+      { new: true }
+    );
+    console.log(updatedDoc);
+  } catch (error) {
+    console.log(error);
+  }
+};
+updateDoc();
+//!----.findOneAndUpdate()---------
+
 //start the server
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
